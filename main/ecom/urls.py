b93b2c06117b3c6dd0883023669269c922613ee2
pankajmanral <pankajmanral0208@gmail.com>
2024-09-01 23:13:26 +0530

    initial

---
+++ main/ecom/urls.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('',views.home,name='home'),
+    path('home/',views.home,name='home'),
+    path('about/',views.about,name='about'),
+    path('contact/',views.contact,name='contact'),
+    path('login/',views.login,name='login'),
+    path('register/',views.register,name='register'),
+]
---
+++ main/ecom/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+
+def home(request):
+    return render(request,'index.html')
+
+def about(request):
+    return render(request,'about.html')
+
+def contact(request):
+    return render(request,'contact.html')
+
+def login(request):
+    return render(request,'login.html')
+
+def register(request):
+    return render(request,'register.html')
+    
